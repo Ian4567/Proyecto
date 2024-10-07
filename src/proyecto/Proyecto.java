b721dcd0dca036f58d0b13733d7533c557e41ea7
Ian <ianloco60@gmail.com>
2024-10-07 12:53:27 +0200

    Primero

---
+++ src/proyecto/Proyecto.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author DAW-11
+ */
+public class Proyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        int opcion = 0;
+        while (opcion != 4) {
+
+            System.out.println("MENU DE OPERACIONES EN JAVA");
+            System.out.println("1- Ejercicio 1");
+            System.out.println("2- Ejercicio 2");
+            System.out.println("3- Ejercicio 3");
+            System.out.println("4- Salir");
+            System.out.println("Elija una opción:……………");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    ejercicio1();
+                    break;
+                case 2:
+                    ejercicio2();
+                    break;
+                case 3:
+                    ejercicio3();
+                    break;
+                case 4:
+                    System.out.println("Has salido");
+                    break;
+                default:
+                    System.out.println("Respuesta no valida,introduce una del 1 al 4");
+            }
+        }
+
+    }
+
+    private static void ejercicio3() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void ejercicio2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private static void ejercicio1() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
